788bae97efa4483a945742b103822e04e9df444b
Pengcheng Yin <pengcheng@fb.com>
2020-05-16 18:32:36 -0400

    revise model loading

--- nsm/parser_module/table_bert_helper.py
+++ nsm/parser_module/table_bert_helper.py
@@ -15,20 +15,21 @@ from nsm.parser_module.sequence_util import StringMatchUtil
 
 
 def get_table_bert_model(config: Dict, use_proxy=False, master=None):
-    tb_path = config.get('table_bert_model')
+    tb_path = config.get('table_bert_model_or_config')
+    if tb_path is None or tb_path == '':
+        tb_path = config.get('table_bert_model')
+    if tb_path is None or tb_path == '':
+        tb_path = config.get('table_bert_config_file')
 
-    if tb_path in ('vertical', 'vanilla'):
-        tb_config_file = config['table_bert_config_file']
-        table_bert_cls = {
-            'vertical': VerticalAttentionTableBert,
-            'vanilla': VanillaTableBert
-        }[tb_path]
+    tb_path = Path(tb_path)
+    assert tb_path.exists()
+
+    if tb_path.suffix == '.json':
+        tb_config_file = tb_path
         tb_path = None
     else:
         print(f'Loading table BERT model {tb_path}', file=sys.stderr)
-        tb_path = Path(tb_path)
         tb_config_file = tb_path.parent / 'tb_config.json'
-        table_bert_cls = TableBertModel
 
     if use_proxy:
         from nsm.parser_module.table_bert_proxy import TableBertProxy
@@ -36,14 +37,24 @@ def get_table_bert_model(config: Dict, use_proxy=False, master=None):
         table_bert_model = TableBertProxy(actor_id=master, table_bert_config=tb_config)
     else:
         table_bert_extra_config = config.get('table_bert_extra_config', dict())
-        table_bert_model = table_bert_cls.load(
-            tb_path,
-            tb_config_file,
-            **table_bert_extra_config
-        )
+        # if it is a not pre-trained model, we use the default parameters
+        if tb_path is None:
+            table_bert_cls = TableBertConfig.infer_model_class_from_config_file(tb_config_file)
+            print(f'Creating a default {table_bert_cls.__name__} without pre-trained parameters!', file=sys.stderr)
 
-        if type(table_bert_model) == VanillaTableBert:
-            table_bert_model.config.column_representation = config.get('column_representation', 'mean_pool')
+            table_bert_model = table_bert_cls(
+                config=table_bert_cls.CONFIG_CLASS.from_file(
+                    tb_config_file, **table_bert_extra_config
+                )
+            )
+        else:
+            table_bert_model = TableBertModel.from_pretrained(
+                tb_path,
+                **table_bert_extra_config
+            )
+
+        # if type(table_bert_model) == VanillaTableBert:
+        #     table_bert_model.config.column_representation = config.get('column_representation', 'mean_pool')
 
         print('Table Bert Config', file=sys.stderr)
         print(json.dumps(vars(table_bert_model.config), indent=2), file=sys.stderr)
